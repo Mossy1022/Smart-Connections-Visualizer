5fed2298ba093c25e6e45bb6310c895205a965ff
WFH Brian <brian@plexm.com>
2024-05-30 09:34:32 -0400

    remove extraneous click event

--- main.ts
+++ main.ts
@@ -1474,12 +1474,6 @@ export default class MyPlugin extends Plugin {
         // This adds a settings tab so the user can configure various aspects of the plugin
         this.addSettingTab(new SampleSettingTab(this.app, this));
 
-        // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-        // Using this function will automatically remove the event listener when this plugin is disabled.
-        this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-            console.log('click', evt);
-        });
-
         // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
         this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
     }
