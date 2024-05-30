fd926dbbfe6cf760b84086ba8c3173dfde59ee3a
WFH Brian <brian@plexm.com>
2024-05-30 09:45:41 -0400

    comment out unused boilerplate

--- main.ts
+++ main.ts
@@ -1439,21 +1439,21 @@ export default class MyPlugin extends Plugin {
                 });
             }
         });
-        // This adds an editor command that can perform some operation on the current editor instance
-        this.addCommand({
-            id: 'sample-editor-command',
-            name: 'Sample editor command',
-            callback: () => {
-                // Create a new leaf in the current workspace
-                let leaf = this.app.workspace.getLeaf(true);
+        // // This adds an editor command that can perform some operation on the current editor instance
+        // this.addCommand({
+        //     id: 'sample-editor-command',
+        //     name: 'Sample editor command',
+        //     callback: () => {
+        //         // Create a new leaf in the current workspace
+        //         let leaf = this.app.workspace.getLeaf(true);
         
-                // Set the new leaf's view to your custom view
-                leaf.setViewState({
-                    type: "My Item View",
-                    active: true,
-                });
-            }
-        });
+        //         // Set the new leaf's view to your custom view
+        //         leaf.setViewState({
+        //             type: "My Item View",
+        //             active: true,
+        //         });
+        //     }
+        // });
 
         // This adds a complex command that can check whether the current state of the app allows execution of the command
         this.addCommand({
@@ -1471,8 +1471,8 @@ export default class MyPlugin extends Plugin {
             }
         });
 
-        // This adds a settings tab so the user can configure various aspects of the plugin
-        this.addSettingTab(new SampleSettingTab(this.app, this));
+        // // This adds a settings tab so the user can configure various aspects of the plugin
+        // this.addSettingTab(new SampleSettingTab(this.app, this));
 
 				// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
         // Using this function will automatically remove the event listener when this plugin is disabled.
@@ -1480,8 +1480,8 @@ export default class MyPlugin extends Plugin {
 					// console.log('click', evt);
 				});
 
-        // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-        this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+        // // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+        // this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
     }
 
     onunload() {}
@@ -1495,28 +1495,28 @@ export default class MyPlugin extends Plugin {
     }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-    plugin: MyPlugin;
+// class SampleSettingTab extends PluginSettingTab {
+//     plugin: MyPlugin;
 
-    constructor(app: App, plugin: MyPlugin) {
-        super(app, plugin);
-        this.plugin = plugin;
-    }
+//     constructor(app: App, plugin: MyPlugin) {
+//         super(app, plugin);
+//         this.plugin = plugin;
+//     }
 
-    display(): void {
-        const {containerEl} = this;
+//     display(): void {
+//         const {containerEl} = this;
 
-        containerEl.empty();
+//         containerEl.empty();
 
-        new Setting(containerEl)
-            .setName('Setting #1')
-            .setDesc('It\'s a secret')
-            .addText(text => text
-                .setPlaceholder('Enter your secret... This will change everything')
-                .setValue(this.plugin.settings.mySetting)
-                .onChange(async (value) => {
-                    this.plugin.settings.mySetting = value;
-                    await this.plugin.saveSettings();
-                }));
-    }
-}
+//         new Setting(containerEl)
+//             .setName('Setting #1')
+//             .setDesc('It\'s a secret')
+//             .addText(text => text
+//                 .setPlaceholder('Enter your secret... This will change everything')
+//                 .setValue(this.plugin.settings.mySetting)
+//                 .onChange(async (value) => {
+//                     this.plugin.settings.mySetting = value;
+//                     await this.plugin.saveSettings();
+//                 }));
+//     }
+// }
